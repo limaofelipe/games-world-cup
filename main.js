0f117af86bc66460ace09668d4d1f7b1c9b0b8ca
limaofelipe <felipe08.tlima@gmail.com>
2022-11-21 00:24:10 -0300

    filter creation by group

--- main.js
+++ main.js
@@ -1,9 +1,9 @@
-function createGame(player1, hour, player2) {
+function createGame(player1, hour, player2, avatar1, avatar2) {
   return `
         <li>
-          <img src="./assets/icons/brazil.svg" alt="Bandeira do ${player1}">
+          <img src="${avatar1}" alt="Bandeira do ${player1}">
           <strong>${hour}</strong>
-          <img src="./assets/icons/argentina.svg" alt="Bandeira do ${player2}">
+          <img src="${avatar2}" alt="Bandeira do ${player2}">
         </li>
         `
 }
@@ -36,18 +36,58 @@ function create(response) {
   for (const [data, jogos] of Object.entries(listDataJogos)) {
     let htmlJogo = ''
     for (const [index, jogo] of jogos.entries()) {
-      htmlJogo += createGame(jogo.nameFisrtCountry, jogo.hourGame, jogo.nameSecondCountry)
+      htmlJogo += createGame(jogo.nameFisrtCountry, jogo.hourGame, jogo.nameSecondCountry, jogo.avatarFisrtCountry, jogo.avatarSecondCountry)
     }
-    
+
     html += createCard(
       data,
       jogos[0].dayWeek,
       htmlJogo
       )
   }
-    
     document.querySelector("#cards").innerHTML = html
+}
+
+
+function createSelectGroup(response) {
+
+  const listGroups = response.data
+  
+  let html ='<option value="initial">Selecione o Grupo</option>'
+  for (const  [,group] of Object.entries(listGroups)) {
+    
+        html += `<option value="${group.id}">${group.name}</option>`
+  }
+
+        document.querySelector("#group-filter").innerHTML = html
+}
+
+function listGroups() {
+  axios.get('http://localhost:3333/groups')
+  .then(response =>  createSelectGroup(response))
+  .catch(error => console.log(error))
+}
 
 
 
+function selectGroup() {
+  document.getElementById("group-filter").addEventListener("change", selectByGroup);
+  
+}
+
+function selectByGroup() {
+  const idElement = document.getElementById("group-filter")
+  let idGroup = idElement.options[idElement.selectedIndex].value;
+  axios.get(`http://localhost:3333/games/${idGroup}/group`)
+  .then(response => create(response))
+  .catch(error => console.log(error))
+  
+}
+
+
+
+function sourceCountry(nameCountry) {
+  axios.post('http://localhost:3333/countries/unique',{name:nameCountry})
+  .then(response =>  createSelectGroup(response))
+  .catch(error => console.log(error))
 }
